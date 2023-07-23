9efa3e55144fc755536d7e6238609059762645eb
Simon Pears <simon@tyk.io>
2023-07-23 20:02:35 +0100

    fix breaking behave test that tests finding missing links

--- features/steps/check_missing_paths_steps.py
+++ features/steps/check_missing_paths_steps.py
@@ -4,6 +4,8 @@ from behave.runner import Context
 from behave import then, when
 from hamcrest import assert_that, equal_to
 
+from pathlib import Path
+
 from docs.cli.check_menu_yaml_links.cli import find_missing_links_in_menu_yaml
 
 
@@ -24,8 +26,8 @@ def step_impl_then_the_following_missing_links_should_be_reported(
     data = json.loads(context.text)
 
     expected_list = data.get("expected", [])
-    expected_paths = {path_dict.get("path") for path_dict in expected_list}
+    expected_paths = {Path(path_dict.get("path")) for path_dict in expected_list}
 
-    result = {str(path) for path in context.missing_links_result}
+    result = {pathTitlePair.path for pathTitlePair in context.missing_links_result}
 
     assert_that(result, equal_to(expected_paths))
